3577f3cf378c6d7f070157067aa1a4acbc07a7ed
Anilkumar7980 <117730085+Anilkumar7980@users.noreply.github.com>
2024-02-28 17:25:39 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -8,15 +8,7 @@ variable "project_id" {
 
 
 
-variable "github_actions_ip_ranges" {
-  description = "A map of GitHub Actions IP ranges to allowlist"
-  type        = map(string)
-  default     = {
-    "actions1" = "192.0.2.0/24",
-    "actions2" = "198.51.100.0/24",
-    "actions3" = "203.0.113.0/24",
-  }
-}
+
 
 variable "service_account" {
   description = "The service account to be used by the node VMs"
